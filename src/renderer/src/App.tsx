458fc032f804319920f1c1d942c529a37f1b07d9
YaelF05 <yaelfrnc@gmail.com>
2025-05-07 22:37:42 -0600

    profileSelection and home screens

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,11 @@
 import Login from '../src/features/auth/pages/login'
 import ProfileSelection from './features/parent/pages/profileSelection'
+import HomePage from './features/parent/pages/home'
 
 function App(): React.JSX.Element {
   return (
     <>
-      <Login />
+      <HomePage />
     </>
   )
 }
--- src/renderer/src/features/parent/pages/profileSelection.tsx
+++ src/renderer/src/features/parent/pages/profileSelection.tsx
@@ -2,72 +2,76 @@ import { FC } from 'react'
 import styles from '../styles/profileSelection.module.css'
 import ProfileAvatar from '@renderer/assets/images/profile-icon-9.png'
 
-interface Profile {
-    id: number
+interface FatherProfile {
+    user_id: number
     name: string
-    type: 'padre' | 'hijo'
-    avatarImage?: string
-}
-
-const ProfileSelection: FC = () => {
-    const navigate = (path: string) => {
-        //Colocar ruta para home
-        console.log(`Navegando a: ${path}`)
+    type: 'FATHER'
+  }
+  
+  interface ChildProfile {
+    child_id: number
+    name: string
+    last_name: string
+  }
+  
+  type Profile = 
+    | { id: number; type: 'FATHER'; name: string; fullData: FatherProfile }
+    | { id: number; type: 'CHILD'; name: string; fullData: ChildProfile };
+  
+  const ProfileSelection: FC = () => {
+    const profiles: Profile[] = [
+      {
+        id: 1,
+        type: 'FATHER',
+        name: 'Mi perfil',
+        fullData: {
+          user_id: 1,
+          name: 'Mi perfil',
+          type: 'FATHER'
+        }
+      },
+      {
+        id: 2,
+        type: 'CHILD',
+        name: 'Si', 
+        fullData: {
+          child_id: 2,
+          name: 'Si', 
+          last_name: 'No'
+        }
+      }
+    ]
+  
+    const handleProfileSelect = (profile: Profile) => {
+      if (profile.type === 'FATHER') {
+        console.log(`Perfil seleccionado: ${profile.name}`)
+      } else {
+        console.log(`Perfil seleccionado: ${profile.name}`)
+      }
     }
   
-    const profiles: Profile[] = [
-        {
-            id: 1,
-            name: 'Mi perfil',
-            type: 'padre',
-        },
-        {
-            id: 2,
-            name: 'Jhon Doe',
-            type: 'hijo',
-        }
-    ]
-
-    const handleProfileSelect = (profile: Profile) => {
-        if (profile.type === 'padre') {
-            //agregar ruta para home
-            navigate('')
-        } else {
-            //agregar ruta para el chamaco
-            console.log('home hijo')
-        }
-  }
-
     return (
-        <div className={styles.profileSelectionPage}>
+      <div className={styles.profileSelectionPage}>
         <h1 className={styles.title}>Bienvenido, seleccione el perfil</h1>
         
         <div className={styles.profilesContainer}>
-            {profiles.map(profile => (
+          {profiles.map(profile => (
             <div 
-                key={profile.id}
-                className={styles.profileCard}
-                onClick={() => handleProfileSelect(profile)}
+              key={profile.id}
+              className={styles.profileCard}
+              onClick={() => handleProfileSelect(profile)}
             >
-                <div className={styles.avatarContainer}>
-                {profile.avatarImage ? (
-                    <img 
-                    src={profile.avatarImage} 
-                    alt={profile.name} 
-                    className={styles.avatar}
-                    />
-                ) : (
-                    <div className={styles.avatarPlaceholder}>
-                    <img src={ProfileAvatar} alt="Profile" className={styles.profileAvatar} />
-                    </div>
-                )}
+              <div className={styles.avatarContainer}>
+                <div className={styles.avatarPlaceholder}>
+                  <img src={ProfileAvatar} alt="Profile" className={styles.profileAvatar} />
                 </div>
-                <p className={styles.profileName}>{profile.name}</p>
+              </div>
+              <p className={styles.profileName}>{profile.name}</p>
             </div>
-            ))}
+          ))}
         </div>
-        </div>
-  )
-}
-
-export default ProfileSelection
+      </div>
+    )
+  }
+  
+  export default ProfileSelection
