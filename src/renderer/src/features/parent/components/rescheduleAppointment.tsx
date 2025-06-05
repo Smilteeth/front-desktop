e4363c79df2bd76b533afcf72b8cf812735180fc
YaelF05 <yaelfrnc@gmail.com>
2025-06-04 22:23:55 -0600

    merge main

--- src/renderer/src/features/parent/components/rescheduleAppointment.tsx
+++ src/renderer/src/features/parent/components/rescheduleAppointment.tsx
@@ -7,7 +7,6 @@ import { getDentistByIdService, DentistResponse } from '../services/dentistServi
 import { createAppointmentService } from '../services/appointmentService'
 import styles from '../styles/rescheduleAppointment.module.css'
 
-
 interface RescheduleAppointmentModalProps {
   isOpen: boolean
   onClose: () => void
